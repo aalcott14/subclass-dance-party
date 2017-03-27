8f7779106bb7344745a16b64c4dff81aaa7b26da
aalcott14 <aalcott14@gmail.com>
2017-03-27 17:30:15 -0500

    Added bouncing dancer

---
+++ src/bouncyDancer.js
@@ -0,0 +1,18 @@
+var BouncyDancer = function(top, left, timeBetweenSteps) {
+  Dancer.call(this, top, left, timeBetweenSteps);
+
+  // we plan to overwrite the step function below, but we still want the superclass step behavior to work,
+  // so we must keep a copy of the old version of this function
+
+  ///var oldStep = this.step;
+};
+
+BouncyDancer.prototype = Object.create(Dancer.prototype);
+BouncyDancer.prototype.constructor = BouncyDancer;
+
+BouncyDancer.prototype.oldStep = Dancer.prototype.step;
+
+BouncyDancer.prototype.step = function() {
+  this.$node.addClass('dancer-bounce');
+
+};
